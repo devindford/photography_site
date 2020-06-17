5361e0c02b73130b7f79cc82c321b33fc5d26328
Devin Ford <devindford@att.net>
2020-06-17 00:37:30 -0400

    added position fixed toggle to burger menu to prevent scrolling when open

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
+const body = document.querySelector("body");
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
   burger.classList.toggle("toggle");
+  body.classList.toggle("fixed");
 });
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
     nav.classList.toggle("nav-open");
     burger.classList.toggle("toggle");
+    body.classList.toggle("fixed");
   });
 });
